f1bb17ae5a9376c4fb982366da74de864bb56ba2
peter <peter@shopmy.com.au>
2020-08-08 04:32:06 +1000

    v3

--- js/custom.js
+++ js/custom.js
@@ -61,3 +61,16 @@ $(window).load(function () {
         }
     });
 });
+
+
+$(window).load(function () {
+    $(".trigger_popup_fricc").click(function(){
+       $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+});
